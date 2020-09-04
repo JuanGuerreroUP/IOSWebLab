63eb8adcfb36c0b7e785ec69bb994204b36f893b
Juan Guerrero <juan.guerrero.v99@gmail.com>
2020-09-04 13:14:43 -0500

    first commit

---
+++ dynamicData.js
@@ -0,0 +1,38 @@
+dynamicData = {
+  "cursos": [
+    {
+      "nombre":"",
+      "imagen":"",
+      "descripcion":""
+    },
+    {
+      "nombre":"",
+      "imagen":"",
+      "descripcion":""
+    }
+  ],
+  "apps": [
+    {
+      "nombre":"",
+      "imagen":"",
+      "descripcion":""
+    },
+    {
+      "nombre":"",
+      "imagen":"",
+      "descripcion":""
+    }
+  ],
+  "blog": [
+    {
+      "nombre":"",
+      "imagen":"",
+      "descripcion":""
+    },
+    {
+      "nombre":"",
+      "imagen":"",
+      "descripcion":""
+    }
+  ]
+}
